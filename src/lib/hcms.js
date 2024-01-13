dc307b0a49dd0de69ef5167a20d8e67c2124465d
Grzegorz Skorupa <g.skorupa@gmail.com>
2024-01-14 00:45:42 +0100

    initial release

---
+++ src/lib/hcms.js
@@ -0,0 +1,73 @@
+export const hcms = {
+    getDocuments: async function (devMode, serviceUrl, path, indexFileName) {
+        console.log("hcms.getDocuments: devMode=" + devMode)
+        let docs = [
+            { content:"This is <b>test</b> doc", path: "/doc1" },
+        ]
+        if (devMode) {
+            return { paths: this.getPaths(path.params.file), documents: docs }
+        }
+        let method = 'GET'
+        let url = serviceUrl + "?path="
+        if(path.params.file.length==0 && indexFileName!=undefined){
+            url=url+indexFileName
+        }else{
+            url=url+path.params.file
+        }
+        console.log("hcms.getDocuments: url=" + url)
+        const headers = new Headers()
+        headers.set('Accept', 'application/json');
+        const response = await fetch(url, { method: method, mode: 'cors', headers: headers })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText)
+                }
+                let documents = response.json()
+                return {
+                    paths: this.getPaths(path.params.file),
+                    documents: documents
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error)
+                return {}
+            })
+        return response
+    },
+    getPaths: function (docpath) {
+        let paths = docpath.split("/")
+        console.log("hcms.getPaths: paths=" + paths)
+        paths.splice(0, 0, "")
+        let result = []
+        result.push({
+            name: "", //home folder on index 0
+            path: "/"
+        })
+        for (let i = 1; i < paths.length; i++) {
+            if(paths[i].length==0){
+                continue
+            }
+            if (paths[i].indexOf(".") > 0) {
+                result.push({
+                    name: paths[i],
+                    path: result[i - 1].path + paths[i]
+                })
+            } else {
+                result.push({
+                    name: paths[i],
+                    path: result[i - 1].path + paths[i] // + "/"
+                })
+            }
+
+        }
+        return result
+    },
+    getElementPath: function (index, paths) {
+        let path = ""
+        for (let i = 0; i < index; i++) {
+            path += paths[i]
+        }
+        return path
+    }
+
+}
---
+++ src/routes/[...file]/+page.svelte
@@ -0,0 +1,37 @@
+<div class="row container-fluid">
+    <div class="col mb-2">
+        {#await data}
+        ...
+        {:then data}
+        <!-- breadcrumbs -->
+        {#if data.paths!=undefined && data.paths!=null && data.paths.length>0}
+        {#each data.paths as pathelem, index}
+        <a href={pathelem.path}>{pathelem.name==''?'home':pathelem.name}</a> {data.paths.length-1>index?' / ':''}
+        {/each}
+        {/if}
+        {/await}
+    </div>
+</div>
+<div class="row container-fluid">
+    <div class="col">
+        <div class="row w-100">
+            <div class="col">
+                {#await data}
+                ...
+                {:then result}
+                {#if result!=undefined && result!=null && result.documents!=null && result.documents!=undefined && result.documents.length>0}
+                {@html result.documents[0].content}
+                {:else}
+                <h1>404</h1>
+                <p>Page not found.</p>
+                {/if}
+                {:catch error}
+                {error.message}
+                {/await}
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    export let data
+</script>
