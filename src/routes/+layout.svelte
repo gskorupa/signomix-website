076bab8a6d954737e58415d2c92c18b4136fdfdb
Grzegorz Skorupa <g.skorupa@gmail.com>
2024-02-20 00:51:33 +0100

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,42 @@
-    <div class="container-fluid pe-0" style="background-color: #0d6efd;"> <!--#1849cf-->
-        <!--
+<div class="container-fluid pe-0" style="background-color: #0d6efd;"> <!--#1849cf-->
+    <!--
         <a class="navbar-brand ms-2" href="#">
             <img src="/logo-light.svg" alt="Logo" height="36" class="d-inline-block my-auto me-2">
             <span class="h3 my-auto" style="color: rgb(223, 223, 223);">Signomix</span>
         </a>
         -->
-        <div class="row w-100">
-            <div class="col-6 h3 my-auto py-2">
-                <img src="/logo-light.svg" alt="Logo" height="36"  class="d-none d-sm-inline">
-                <span style="color: rgb(255, 255, 255);">Signomix</span>
-            </div>
-            <div class="col-6 text-end my-auto" style="color: rgb(255, 255, 255);">
-                <a href="/pl/index.html" class="link-light underline-opacity-0 underline-opacity-hover-0">
-                    <img src="/flags/4x3/pl.svg" height="16px" title="polski"/>
-                </a> 
-                <a href="/en/index.html" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2">
-                    <img src="/flags/4x3/gb.svg" height="16px" title="english"/>
-                </a>
-            </div>
+    <div class="row w-100">
+        <div class="col-6 h3 my-auto py-2">
+            <img src="/logo-light.svg" alt="Logo" height="36" class="d-none d-sm-inline">
+            <span style="color: rgb(255, 255, 255);">Signomix</span>
+        </div>
+        <div class="col-6 text-end my-auto" style="color: rgb(255, 255, 255);">
+            <a href="/pl/index.html" class="link-light underline-opacity-0 underline-opacity-hover-0">
+                <img src="/flags/4x3/pl.svg" height="16px" title="polski" />
+            </a>
+            <a href="/en/index.html" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2">
+                <img src="/flags/4x3/gb.svg" height="16px" title="english" />
+            </a>
         </div>
     </div>
+</div>
 <div class="container-fluid">
-<slot></slot>
+    <slot></slot>
 </div>
 <div class="container-fluid">
     <footer class="py-2 my-3 border-top">
-        <div class="row ">
-            <div class="col-12">
-                <a href="https://experiot.pl" target="_blank" class="text-body-secondary">&copy; 2024 EXPERIOT</a>
+        <div class="row text-center">
+            <div class="col">
+                <a href="mailto:signomix@experiot.pl"><i class="bi bi-envelope-at h4 mx-1"></i></a>
+                <a href="https://discord.gg/ATMKpZyePm"><i class="bi bi-discord h4 mx-1"></i></a>
+                <a href="https://twitter.com/SignomixIoT"><i class="bi bi-twitter h4 mx-1"></i></a>
+                <a href="https://github.com/signomix"><i class="bi bi-github h4 mx-1"></i></a>
+                <a href="https://www.linkedin.com/company/experiot"><i class="bi bi-linkedin h4 mx-1"></i></a>
             </div>
             <!--
+            <div class="col">
+                <a href="https://experiot.pl" target="_blank" class="text-body-secondary">&copy; 2024 EXPERIOT</a>
+            </div>
             <div class="col-9 text-end">
                 <a class="text-body-secondary" href="https://github.com/gskorupa/cricket-website-template" target="_blank"><i class="bi bi-github h4 text-black-50"></i></a>
             </div>
