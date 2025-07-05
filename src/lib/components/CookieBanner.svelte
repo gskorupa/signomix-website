0ed5cc11d7d7d313ee99d23c8aea525203fc2a96
Grzegorz Skorupa <g.skorupa@gmail.com>
2025-07-06 00:26:08 +0200

    Add multilingual support to cookie banner and update footer links

--- src/lib/components/CookieBanner.svelte
+++ src/lib/components/CookieBanner.svelte
@@ -1,6 +1,7 @@
 <div id="cookie-banner"
     style="position:fixed;bottom:0;left:0;width:100%;background:#f8f9fa;border-top:1px solid #ccc;padding:16px;z-index:1000;display:none;">
     <div class="row">
+        {#if language === 'pl'}
         <div class="col-md-10">
             Strony serwisu signomix.com używają plików cookies w celach technicznych (niezbędnych do działania tych
             stron)
@@ -13,12 +14,26 @@
             <button id="cookie-accept" class="btn btn-success btn-sm ms-2 m-2">Akceptuję</button>
             <button id="cookie-reject" class="btn btn-secondary btn-sm ms-2 m-2">Odrzucam</button>
         </div>
+        {:else}
+        <div class="col-md-10">
+            This website uses cookies for technical purposes (necessary for the operation of the site) and statistical
+            purposes (allowing us to collect anonymous data about traffic on the site).<br>
+            You can accept or reject cookies that are not necessary for the operation of the site.<br>
+            <a href="https://documentation.signomix.com/en/legal/terms_and_conditions.md" target="_blank">Learn
+                more</a>.
+        </div>
+        <div class="col-md-2 text-end">
+            <button id="cookie-accept" class="btn btn-success btn-sm ms-2 m-2">Accept</button>
+            <button id="cookie-reject" class="btn btn-secondary btn-sm ms-2 m-2">Reject</button>
+        </div>
+        {/if}
     </div>
 </div>
 <script>
     import { onMount } from 'svelte';
     import { browser } from '$app/environment';
 
+    export let language = 'en'; // Domyślny język, można zmienić na 'pl' jeśli potrzebne
     onMount(() => {
         if (browser) {
             document.addEventListener("DOMContentLoaded", function () {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,12 +50,18 @@
                 <a href="/pl/blog/index.md" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2">
                     Blog
                 </a>
+                <a href="https://documentation.signomix.com/pl/index.md" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2" target="_blank">
+                    Dokumentacja
+                </a>
                 {:else}
                 <a href="/en/index.html" class="link-light underline-opacity-0 underline-opacity-hover-0">
                     Home
                 </a>
                 <a href="/en/blog/index.md" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2">
                     Blog
+                </a>
+                <a href="https://documentation.signomix.com/en/index.md" class="link-light underline-opacity-0 underline-opacity-hover-0 ms-2" target="_blank">
+                    Documentation
                 </a>
                 {/if}
             </div>
@@ -84,15 +90,25 @@
 </div>
 <div class="container-fluid">
     <footer class="py-2 my-3 border-top">
-        <div class="row text-center">
-            <div class="col">
-                <a href="mailto:signomix@experiot.pl"><i class="bi bi-envelope-at h4 mx-1"></i></a>
-                <a href="https://discord.gg/ATMKpZyePm"><i class="bi bi-discord h4 mx-1"></i></a>
-                <a href="https://x.com/SignomixIoT"><i class="bi bi-twitter-x h4 mx-1"></i></a>
-                <a href="https://github.com/signomix"><i class="bi bi-github h4 mx-1"></i></a>
-                <a href="https://www.linkedin.com/company/experiot"><i class="bi bi-linkedin h4 mx-1"></i></a>
+        <div class="row">
+            <div class="col text-start">
+                <span class="fs-6 ms-2">&copy; 2025 Experiot</span>
+            </div>
+            <div class="col  text-center">
+                <a href="mailto:signomix@experiot.pl"><i class="bi bi-envelope-at h5 mx-1"></i></a>
+                <a href="https://discord.gg/ATMKpZyePm"><i class="bi bi-discord h5 mx-1"></i></a>
+                <a href="https://x.com/SignomixIoT"><i class="bi bi-twitter-x h5 mx-1"></i></a>
+                <a href="https://github.com/signomix"><i class="bi bi-github h5 mx-1"></i></a>
+                <a href="https://www.linkedin.com/company/experiot"><i class="bi bi-linkedin h5 mx-1"></i></a>
+            </div>
+            <div class="col text-end">
+                {#if isPlVersion($page.url.pathname)}
+                <span class="fs-6 me-2"><a href="https://documentation.signomix.com/pl/legal/terms_and_conditions.md" target="_blank">Warunki i prywatność</a></span>
+                {:else}
+                <span class="fs-6 me-2"><a href="https://documentation.signomix.com/en/legal/terms_and_conditions.md" target="_blank">Terms & Privacy</a></span>
+                {/if}
             </div>
         </div>
     </footer>
 </div>
-<CookieBanner />
+<CookieBanner language={isPlVersion($page.url.pathname)?'pl':'en'} />
